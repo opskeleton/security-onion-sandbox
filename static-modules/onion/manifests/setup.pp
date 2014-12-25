d68611dd8a3c27179dd55bda429d814901023031
ronen <narkisr@gmail.com>
2014-12-25 02:34:17 +0200

    fixing network and general setup order

--- static-modules/onion/manifests/setup.pp
+++ static-modules/onion/manifests/setup.pp
@@ -1,7 +1,7 @@
 # Setting up security onion using sosetup
 class onion::setup(
   $mgmt_ifc = 'eth0',
-  $sniff_ifc = 'eth2',
+  $sniff_ifc = 'eth1',
   $user,
   $password,
   $email
@@ -15,10 +15,20 @@ class onion::setup(
     group => root,
   } ->
 
-  exec{'first so setup run':
-    command => 'echo yes | sosetup -f /tmp/sosetup.conf >> /var/log/sosetup.log',
-    user    => 'root',
-    path    => ['/usr/bin','/bin',]
-  }
+  exec{'so global setup run':
+    command     => 'echo yes | sosetup -f /tmp/sosetup.conf >> /var/log/sosetup.log && touch /var/sosetup-skip.run',
+    user        => 'root',
+    environment => 'HOME=/home/vagrant',
+    path        => ['/usr/bin','/bin',],
+    onlyif      => 'grep promisc /etc/network/interfaces',
+    unless      => 'test -f /var/sosetup-skip.run'
+  } ->
 
+  exec{'so network setup run':
+    command     => 'yes | sosetup-network -f /tmp/sosetup.conf >> /var/log/sosetup-network.log',
+    user        => 'root',
+    environment => 'HOME=/home/vagrant',
+    path        => ['/usr/bin','/bin',],
+    unless      => 'grep promisc /etc/network/interfaces'
+  }
 }
