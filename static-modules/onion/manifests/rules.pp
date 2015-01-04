d9416cea3a467469f844e4fc2b0b5cee303f0d2f
narkisr <narkisr@gmail.com>
2015-01-04 04:44:06 +0200

    oink code support and torrent rule ignore

--- static-modules/onion/manifests/rules.pp
+++ static-modules/onion/manifests/rules.pp
@@ -1,12 +1,16 @@
 # nsm rules management
 class onion::rules(
-  $disabled=['dropbox_usage']
+  $disabled=['dropbox_usage'],
+  $oinkcode= '',
 ) {
   $ids = {
     'dropbox_usage'     => '2012647',
     'dropbox_broadcast' => '2012648',
-    'apt_outbound'      => '2013504'
+    'apt_outbound'      => '2013504',
+    'torrent_peer'      => '2000334'
   }
+
+  $parent = 'https://www.snort.org/reg-rules/'
 
   file { '/etc/nsm/pulledpork/disablesid.conf':
     ensure  => file,
@@ -21,5 +25,17 @@ class onion::rules(
     refreshonly => true,
     user        => 'root',
     path        => ['/usr/bin','/bin'],
+  }
+
+  if($oinkcode !=''){
+    file_line { 'snapshots url':
+      path => '/etc/nsm/pulledpork/pulledpork.conf',
+      line => "${parent}|snortrules-snapshot.tar.gz|${oinkcode}"
+    } ~> Exec['update snort rules']
+
+    file_line { 'opensource url':
+      path => '/etc/nsm/pulledpork/pulledpork.conf',
+      line => "${parent}|opensource.tar.gz|${oinkcode}",
+    } ~> Exec['update snort rules']
   }
 }
