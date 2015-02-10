eb733d82785fa03271ec5abc026950cb1b83ee37
ronen <narkisr@gmail.com>
2015-02-10 21:09:42 +0200

    supporting a mountable nsm partition

--- static-modules/onion/manifests/mount.pp
+++ static-modules/onion/manifests/mount.pp
@@ -1,26 +1,33 @@
 # Replacing /nsm mount point see
 # https://code.google.com/p/security-onion/wiki/NewDisk
-class onion::mount(
-  $device='',
-  $dest =''
-){
+class onion::mount($device='', $dest =''){
   if($device != '' and $dest !=''){
+    if($dest == '/nsm'){
+      fail('destination path cannot be /nsm')
+    }
     exec{'service nsm stop':
       command => 'service nsm stop',
       user    => 'root',
       path    => ['/usr/bin','/bin',]
     } ->
 
-    exec{'move /nfs aside':
-      command => 'mv /nsm /nsm_bac',
+    file{$dest:
+      ensure => directory,
+    } ->
+
+    mkfs::device {$device:
+      dest   => $dest
+    } ->
+
+    exec{'move /nsm aside':
+      command => "mv /nsm ${dest}",
       user    => 'root',
       path    => '/bin/'
-    }
+    } ->
 
-    class{'mkfs':
-      device => $device,
-      dest => $dest
+    file{'/nsm':
+      ensure => link,
+      target => "${dest}/nsm"
     }
   }
-
 }
