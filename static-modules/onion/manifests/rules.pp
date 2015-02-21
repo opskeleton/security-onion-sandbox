33fb217e008c1d980186eeddf368e670807df9c3
narkisr <narkisr@gmail.com>
2015-02-21 22:41:38 +0200

    more signatures ignored

--- static-modules/onion/manifests/rules.pp
+++ static-modules/onion/manifests/rules.pp
@@ -4,10 +4,15 @@ class onion::rules(
   $oinkcode= '',
 ) {
   $ids = {
-    'dropbox_usage'     => '2012647',
-    'dropbox_broadcast' => '2012648',
-    'apt_outbound'      => '2013504',
-    'torrent_peer'      => '2000334'
+    'dropbox_usage'      => '2012647',
+    'dropbox_broadcast'  => '2012648',
+    'apt_outbound'       => '2013504',
+    'torrent_peer'       => '2000334',
+    'tor_ssl'            => '2018789',
+    'http_non_rfc_char'  => '14',
+    'stream5_reset'      => '15',
+    'http_no_length'     => '3',
+    'http_unkown_method' => '31'
   }
 
   $parent = 'https://www.snort.org/reg-rules/'
